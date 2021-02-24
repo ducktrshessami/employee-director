2e241e6aced6c359ddb57bd2616df9956ac0ba0a
ducktrshessami <jwee761@gmail.com>
2021-02-23 20:23:57 -0800

    created header component

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function Jumbotron() {
+    return (
+        <div className="header text-white text-center p-5">
+            <h1>Employee Directory</h1>
+            <h6>Click on carrots to filter by heading or use the search box to narrow your results.</h6>
+        </div>
+    );
+};
