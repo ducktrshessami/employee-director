4eabe4e7f21848f386bc9a8175d9fa952d988cfb
ducktrshessami <jwee761@gmail.com>
2021-02-23 22:34:55 -0800

    rough app structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Searchbar from "./components/Searchbar";
+import Directory, { Employee } from "./components/Directory";
 
 export default function App() {
+  const [searchValue, setSearch] = useState("");
+  const [employees, setEmployees] = useState([]);
+
   return (
     <div>
       <Header/>
-      <Searchbar/>
+      <Searchbar onChange={event => setSearch(event.target.value)}/>
+      <Directory>
+        {employees.map(Employee)}
+      </Directory>
     </div>
   );
 }
