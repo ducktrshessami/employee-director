027041e53f3321402bd635e1ac06b6fbaecf491d
ducktrshessami <jwee761@gmail.com>
2021-02-24 00:00:16 -0800

    employee item formatting

--- src/components/Directory/Employee.js
+++ src/components/Directory/Employee.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+function zero(n) {
+    return n < 10 ? `0${n}` : `${n}`;
+}
+
+function formatDate(date) {
+    return `${zero(date.getMonth() + 1)}-${zero(date.getDate())}-${date.getFullYear()}`;
+}
+
 export default function Employee({ image, name, phone, email, dob }) {
     return (
-        <li className="row">
+        <li className="row py-2">
             <div className="col-2 m-auto">
                 <img className="employee-img" alt={name} src={image}/>
             </div>
@@ -11,7 +19,7 @@ export default function Employee({ image, name, phone, email, dob }) {
             <div className="col-3 m-auto">
                 <a target="_blank" rel="noreferrer" href={"mailto:" + email}>{email}</a>
             </div>
-            <div className="col-2 m-auto">{dob}</div>
+            <div className="col-2 m-auto">{formatDate(new Date(dob))}</div>
         </li>
     );
 };
